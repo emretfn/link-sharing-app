4a23dc120c51762d2b011c8232fddf8604f97dbd
Emre TUFAN <emretufan42@gmail.com>
2023-08-17 04:27:25 +0300

    feat: add assets and setup tailwind settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          DEFAULT: "#633CFF",
+          hover: "#BEADFF",
+          light: "#EFEBFF",
+        },
+        grey: {
+          DEFAULT: "#737373",
+          dark: "#333333",
+          light: "#FAFAFA",
+          border: "#D9D9D9",
+        },
+        white: "#FFFFFF",
+        red: "#FF3939",
+      },
+      fontSize: {
+        sm: "0.75rem",
+        "4xl": "2rem",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
