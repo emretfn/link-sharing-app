f740d4a7b5e13d912ee282659ffa74c2a51b385b
Emre TUFAN <emretufan42@gmail.com>
2023-08-25 01:04:59 +0300

    refactor: change font files

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,7 @@ import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
 const instrumentSans = localFont({
-  src: [
-    {
-      path: "../public/assets/fonts/InstrumentSans-Bold.ttf",
-      weight: "700",
-    },
-    {
-      path: "../public/assets/fonts/InstrumentSans-Regular.ttf",
-      weight: "400",
-    },
-  ],
+  src: "../public/assets/fonts/InstrumentSans-VariableFont_wdth,wght.ttf",
 });
 
 export const metadata: Metadata = {
