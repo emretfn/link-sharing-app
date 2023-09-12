67218f6b3a60567a77e7e64b9940f0201469725d
Emre Tufan <emretufan42@gmail.com>
2023-09-12 17:48:05 +0300

    refactor: refactor caption component to add classnames

--- components/caption.tsx
+++ components/caption.tsx
@@ -5,16 +5,26 @@ interface CaptionProps {
   title: string;
   description: string;
   align?: "left" | "center" | "right";
+  className?: string;
 }
 
-const Caption = ({ title, description, align = "left" }: CaptionProps) => {
+const Caption = ({
+  title,
+  description,
+  align = "left",
+  className,
+}: CaptionProps) => {
   return (
     <div
-      className={cn("flex flex-col gap-y-2", {
-        "text-center": align === "center",
-        "text-right": align === "right",
-        "text-left": align === "left",
-      })}
+      className={cn(
+        "flex flex-col gap-y-2",
+        {
+          "text-center": align === "center",
+          "text-right": align === "right",
+          "text-left": align === "left",
+        },
+        className
+      )}
     >
       <p className="heading-m">{title}</p>
       <p className="text-grey">{description}</p>
